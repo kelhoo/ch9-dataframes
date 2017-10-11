fa16d90385a6d06f26f8488f616ee18e59262a76
Kelsey Hoover <kelphoover@gmail.com>
2017-10-11 12:05:28 -0700

    created data frame of seahawks game scores, wins and opponents

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,20 +4,21 @@
                            # Hint: google "Seahawks scores", or check the football database:
                            # http://www.footballdb.com/teams/nfl/seattle-seahawks/results
                            # here 'Result' displays scores, 'L' stands for loss, 'W' for win
-
+seahawksscore <- c(48, 20, 26, 17, 9, 12, 27, 46, 16)
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 5 games
-
+seaopponentscore <- c(17, 13, 13, 13, 17, 9, 33, 18, 10)
 
 # Combine your two vectors into a dataframe
-
+season <- data.frame(seahawksscore, seaopponentscore)
 
 # Create a new column "diff" that is the difference in points
-
+season$diff <- seahawksscore - seaopponentscore
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+season$win <- seahawksscore > seaopponentscore
 
 # Create a vector of the opponents
-
+opponents <- c("Chargers", "Vikings", "Chiefs", "Raiders", "Packers", "49ers", "Titans", "Colts", "Rams")
 
 # Add the vector of opponents into the data frame
+season$opponent <- opponents
